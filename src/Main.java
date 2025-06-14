d95032141a230a2a63f959794fa2bffffacad6d9
Mbathio Lo <lombathio356@gmail.com>
2025-06-13 22:40:19 -0400

    Task 2 Completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //class Month
+        //main()
+        //num birthMonth
+        //output "Enter your birth month [1-12]: "
+        //input birthMonth
+        //if birthMonth >= 1 AND birthMonth <= 12 then
+        //   output "Your birth month is: " + birthMonth
+        //else
+        //   output "You entered an incorrect month value: " + birthMonth
+        //end if
+        //return
+        //end class
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int birthMonth = 0;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your birth month [1-12]: ");
+        if (in.hasNextInt()) {
+            birthMonth = in.nextInt();
+            in.nextLine();
+
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+            else {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
+        }
+        else {
+            String trash = in.nextLine();
+            System.out.println("Invalid input: " + trash);
         }
     }
 }
